887768f66d7e2e88794e81485d2a8447158dc961
hoanganh203 <105597730+hoanganh203@users.noreply.github.com>
2023-08-02 20:21:34 +0700

    fig layout home

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
 import './App.css'
-import viteLogo from '/vite.svg'
 import './App.css'
 import { Route, Routes } from 'react-router-dom'
-import Home from './pages/Home/Home'
+import Home from './pages/Home'
 import LayOutAdmin from './Admin/LayoutAdmin/LayOutAdmin'
 
 function App() {
   return (
     <>
-     <Routes>
-      <Route path='/' element={<Home/>} />
-      <Route path='/admin' element={<LayOutAdmin/>} />
-     </Routes>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/admin' element={<LayOutAdmin />} />
+      </Routes>
     </>
   )
 }
---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,16 @@
+import LayoutUser from "../../component/LayoutUser/LayoutUser";
+import Footer from "../../component/Footer/Footer";
+import ListProductsHome from "../../component/ListProductsHome/ListProductsHome";
+
+
+const Home = () => {
+  return (
+    <>
+      <LayoutUser />
+      <ListProductsHome />
+      <Footer />
+    </>
+  );
+};
+
+export default Home;
