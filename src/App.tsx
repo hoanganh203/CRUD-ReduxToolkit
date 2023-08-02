b3f33781ab15c0e914c442385527ab0975ed961b
nam <nam@gmail.com>
2023-08-02 19:50:30 +0700

    FE: Create Admin

--- src/App.tsx
+++ src/App.tsx
@@ -5,12 +5,14 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import { Route, Routes } from 'react-router-dom'
 import Home from './pages/Home/Home'
+import LayOutAdmin from './Admin/LayoutAdmin/LayOutAdmin'
 
 function App() {
   return (
     <>
      <Routes>
       <Route path='/' element={<Home/>} />
+      <Route path='/admin' element={<LayOutAdmin/>} />
      </Routes>
     </>
   )
